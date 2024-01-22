967b6ac31b8da3386e8927d03fcc7b90a1693d9a
Muhammad Nur Alfian Syarif <222112218@stis.ac.id>
2024-01-22 13:00:40 +0700

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div>
+      <h1 class="text-red-700 text-2xl text-center">Halooo</h1>
+      <button class="btn btn-secondary">Klik ini!</button>
+    </div>
+  </main>
+</template>
